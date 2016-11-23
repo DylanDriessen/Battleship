4ace65c2ee6816f73c73b6f925027be731a4371d
Wouter Cypers <wouter.cypers@gmail.com>
2016-11-23 09:09:05 +0100

    ZeeslagApp & ZeeslagController

--- src/application/ZeeslagApp.java
+++ src/application/ZeeslagApp.java
@@ -1,9 +1,17 @@
 package application;
 
-public class ZeeslagApp {
+import javax.swing.JFrame;
 
+import controller.ZeeslagController;
+import view.SpelFrame;
+
+public class ZeeslagApp {
+	
 	public static void main(String[] args) {
-		//TODO
+		SpelFrame view = new SpelFrame();
+		view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		view.setVisible(true);
+		ZeeslagController controller = new ZeeslagController(view);
 	}
 	
 }
--- src/controller/ZeeslagController.java
+++ src/controller/ZeeslagController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import view.SpelFrame;
+
 public class ZeeslagController {
+
+	private SpelFrame view;
+	
+	public ZeeslagController(SpelFrame view) {
+		this.view = view;
+	}
 
 }
