0c260795affabf5338ad8b7cb8dc0927fd2ecf91
DylanDriessen <dylandriessen1@gmail.com>
2016-12-04 00:40:29 +0100

    ShipFactory changed to ShipType

--- src/model/player/placestrategy/RandomPlaceStrategy.java
+++ src/model/player/placestrategy/RandomPlaceStrategy.java
@@ -11,29 +11,24 @@ import model.enums.ShipType;
 public class RandomPlaceStrategy implements PlaceStrategy {
 	
 	@Override
-	public void placeShip() throws ModelException {
+	public void placeShip() {
 		
 		Random r = new Random();
-		ShipFactory sf = new ShipFactory();
 		
 		int succeededCount = 0;
 		while (succeededCount < 5){
 			
 			int shipTemplateIndex = r.nextInt(ShipType.values().length);
-			int xCoord = r.nextInt(9);
-			int yCoord = r.nextInt(9);
+			int xCoord = r.nextInt(9)-1;
+			int yCoord = r.nextInt(9)-1;
 			int orientationIndex = r.nextInt(Orientation.values().length);
 			
-			Ship ship = ShipFactory.createShipFromTemplate(
-					ShipType.values()[shipTemplateIndex], 
-					xCoord, 
-					yCoord, 
-					Orientation.values()[orientationIndex]);
+			Ship ship = new Ship(shipTemplateIndex, xCoord, yCoord, orientationIndex);
 			
 			try {
 				//Ship plaatsen
 				succeededCount++;
-			} catch (Exception ignored){
+			} catch (ModelException ignored){
 				
 			}
 		}
